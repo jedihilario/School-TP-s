a78e4a7a4e321311a79828c056f7311f32311088
jedihilario <hyanezbertola@gmail.com>
2024-03-13 00:38:44 -0300

    Fixed case problem on load and search

--- files/utils/search.py
+++ files/utils/search.py
@@ -2,6 +2,7 @@ from models.persona import Persona
 
 def search_by_name (people: list[Persona], name: str) -> list[str, int]:
     result: list[str, int] = ['Resultado:\n', 0]
+    name = name.upper()
 
     for i, person in enumerate(people):
         if (person.name == name):
