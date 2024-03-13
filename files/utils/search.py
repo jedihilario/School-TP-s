b88734960d9888431b4969433263cea4a62c7a4c
jedihilario <hyanezbertola@gmail.com>
2024-03-12 21:20:23 -0300

    Improved search message

--- files/utils/search.py
+++ files/utils/search.py
@@ -8,6 +8,8 @@ def search_by_name (people: list[Persona], name: str) -> list[str, int]:
             result[0] += f'Nombre: {person.name} | Apellido: {person.surname} | DNI: {person.dni} | Edad: {person.age}\n\n'
             result[1] = i
 
+    if (result[0] == 'Resultado:\n'): result[0] = 'No se encontro :('
+
     return result
 
 def search_by_dni (people: list[Persona], dni: int) -> int:
