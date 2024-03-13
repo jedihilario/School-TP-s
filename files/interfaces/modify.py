0cb907d2f2e3804359b181ce71df99bc4247559d
jedihilario <hyanezbertola@gmail.com>
2024-03-12 21:17:11 -0300

    Modify type errors handled

--- files/interfaces/modify.py
+++ files/interfaces/modify.py
@@ -3,10 +3,20 @@ from tkinter import ttk
 from models.persona import Persona
 from utils.search import search_by_dni
 
-def modify (people: list[Persona], dni: int, new_data: map, op_var: StringVar) -> None:
+def modify (people: list[Persona], dni: IntVar, new_data: map, op_var: StringVar) -> None:
+    try:
+        dni = dni.get()
+        new_data['name'] = new_data['name'].get()
+        new_data['surname'] = new_data['surname'].get()
+        new_data['dni'] = new_data['dni'].get()
+        new_data['age'] = new_data['age'].get()
+    except:
+        op_var.set('Error: Datos invalidos'); return
+    
     index = search_by_dni(people, dni)
 
-    if (index == -1): op_var.set('Error'); return
+    if (index == -1): op_var.set('Error: Persona no encontrada'); return
+
 
     if (new_data['name'] == ''): new_data['name'] = people[index].name
     if (new_data['surname'] == ''): new_data['surname'] = people[index].surname
@@ -42,8 +52,8 @@ def modify_menu (parent: Tk, people: list[Persona]):
     ttk.Label(window, text = 'Nueva edad:').pack(pady = 2)
     new_age = IntVar()
     age_entry = ttk.Entry(window, textvariable = new_age).pack(pady = 2)
-    ttk.Button(window, text = 'Modificar', command = lambda: modify(people, search_var.get(), new_data = {
-        'name': new_name.get(), 'surname': new_surname.get(), 'dni': new_dni.get(), 'age': new_age.get()
-    }, op_var = op_msg)).pack(pady = 2)
+    ttk.Button(window, text = 'Modificar', command = lambda: modify(people, search_var, {
+        'name': new_name, 'surname': new_surname, 'dni': new_dni, 'age': new_age
+    }, op_msg)).pack(pady = 2)
     ttk.Button(window, text = 'Cancelar', command = lambda: window.destroy()).pack(pady = 2)
     ttk.Label(window, textvariable = op_msg, font = ('Helvetica', 14)).pack(pady = 2)
